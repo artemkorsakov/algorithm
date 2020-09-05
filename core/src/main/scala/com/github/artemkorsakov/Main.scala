364ab6f5cc988c4c1df09c6a0db218c1885edf70
Artem Korsakov <kor@infra.guru>
2020-09-05 15:32:58 +0300

    Initial commit

---
+++ core/src/main/scala/com/github/artemkorsakov/Main.scala
@@ -0,0 +1,6 @@
+package com.github.artemkorsakov
+
+
+object Main {
+  def main(args: Array[String]): Unit = println("Hello World")
+}
---
+++ tests/src/test/scala/com/github/artemkorsakov/tests/AlgorithmTestSuite.scala
@@ -0,0 +1,11 @@
+package com.github.artemkorsakov
+package tests
+
+import org.scalatest.Matchers
+import org.scalatest.funsuite.AnyFunSuiteLike
+
+class AlgorithmTestSuite extends AnyFunSuiteLike with Matchers {
+  test("test") {
+    1 shouldBe 1
+  }
+}
