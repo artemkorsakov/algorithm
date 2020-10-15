32370c6c55123bf881f9d455afc6e01aff6b50b9
Артём <artemkorsakov@mail.ru>
2020-10-15 17:35:38 +0300

    Travis configurate

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Installing Ruby environment..."
+rvm use 2.6.5 --install --fuzzy
+gem update --system
+
+echo "Installing Jekyll..."
+gem install jekyll -v 4
--- scripts/publishMicrosite.sh
+++ scripts/publishMicrosite.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+echo "Publishing documentation..."
 git config --global user.email "artemkorsakov@mail.ru"
 git config --global user.name "artemkorsakov"
 git config --global push.default simple
