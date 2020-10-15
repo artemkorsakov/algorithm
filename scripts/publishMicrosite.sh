0446e0a369a9cfa67e7bc0bd7fd50bbc77ae7b8e
Артём <artemkorsakov@mail.ru>
2020-10-15 16:20:35 +0300

    Travis config

--- scripts/publishMicrosite.sh
+++ scripts/publishMicrosite.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 set -e
 
-git config --global user.email artemkorsakov@mail.ru
-git config --global user.name artemkorsakov
+git config --global user.email "artemkorsakov@mail.ru"
+git config --global user.name "artemkorsakov"
 git config --global push.default simple
 
 sbt publishMicrosite
