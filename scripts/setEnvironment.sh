9dfe925045648a0b3b02b145d386c31619568a95
Артём <artemkorsakov@mail.ru>
2020-10-16 10:17:32 +0300

    Travis snapshot

--- scripts/setEnvironment.sh
+++ scripts/setEnvironment.sh
@@ -12,11 +12,14 @@ git config --global push.default simple
 
 echo "Set gpg parameters..."
 gpg --version
-sed -i -e "use-agent" \
--e "pinentry-mode loopback" \
--e "s/\(default-key \).*/\1$GPG_DEFAULT_KEY/" \
--e "s/\(passphrase \).*/\1$GPG_PASSPHRASE/" ~/.gnupg/gpg.conf
-sed -i 'allow-loopback-pinentry' ~/.gnupg/gpg-agent.conf
-echo RELOADAGENT | gpg-connect-agent
+
+sed -i -e 's/#default-key .*/default-key '$GPG_DEFAULT_KEY' \n passphrase '$GPG_PASSPHRASE'/g' ~/.gnupg/gpg.conf
 cat ~/.gnupg/gpg.conf
-cat ~/.gnupg/gpg-agent.conf
+
+# sed -i 'allow-loopback-pinentry' ~/.gnupg/gpg-agent.conf
+
+# cat ~/.gnupg/gpg-agent.conf
+
+# echo RELOADAGENT | gpg-connect-agent
+
+
