56ec46a9a49fd81d4f1c2bd54173d5ec7bcf98f6
Артём <artemkorsakov@mail.ru>
2020-10-16 11:18:12 +0300

    Travis

--- scripts/setEnvironment.sh
+++ scripts/setEnvironment.sh
@@ -10,16 +10,21 @@ git config --global user.email "artemkorsakov@mail.ru"
 git config --global user.name "artemkorsakov"
 git config --global push.default simple
 
+echo "gpg install..."
+apt update && apt upgrade
+apt install gnupg pbuilder ubuntu-dev-tools apt-file
 echo "gpg version..."
 gpg --version
+echo "import gpg keys..."
 openssl aes-256-cbc -K $encrypted_404964577bd1_key -iv $encrypted_404964577bd1_iv -in all.gpg.enc -out all.gpg -d
 gpg --import all.gpg
+echo "list secret gpg keys..."
 gpg --list-secret-keys
 
 echo "gpg.conf..."
 file=~/.gnupg/gpg.conf
 echo "use-agent" >$file
-# echo "pinentry-mode loopback" >>$file
+echo "pinentry-mode loopback" >>$file
 echo "default-key $GPG_DEFAULT_KEY" >>$file
 echo "passphrase $GPG_PASSPHRASE" >>$file
 cat $file
