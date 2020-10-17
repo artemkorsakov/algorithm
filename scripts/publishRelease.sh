53382242fadf4ffcf002993717f9b06b227bd7e8
Артём <artemkorsakov@mail.ru>
2020-10-17 12:08:06 +0300

    Travis config

--- scripts/publishRelease.sh
+++ scripts/publishRelease.sh
@@ -3,5 +3,5 @@ set -e
 
 echo "Publishing release..."
 git checkout master
-sbt 'release release-version 0.0.15'
+sbt 'release release-version 0.0.15 next-version 0.0.16-SNAPSHOT'
 echo "Publishing release success..."
