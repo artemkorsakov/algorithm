84ebc1a36f4563c2f8475c46d1301b5330faa298
Артём <artemkorsakov@mail.ru>
2020-11-18 09:36:09 +0300

    0.1.1 release - docs refactors

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-
 echo "Installing and configuration gpg..."
 echo "Go to dist folder"
 mkdir dist
 cd dist
-
 echo "Install build-essential"
 sudo apt-get install build-essential make zlib1g-dev
-
 echo "Install Libgpg-error"
 wget https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.39.tar.bz2
 tar jxf libgpg-error-1.39.tar.bz2
@@ -16,7 +13,6 @@ cd libgpg-error-1.39
 make
 sudo make install
 cd ..
-
 echo "Install Libgcrypt"
 wget https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.8.6.tar.bz2
 tar jxf libgcrypt-1.8.6.tar.bz2
@@ -25,7 +21,6 @@ cd libgcrypt-1.8.6
 make
 sudo make install
 cd ..
-
 echo "Install Libksba"
 wget https://www.gnupg.org/ftp/gcrypt/libksba/libksba-1.4.0.tar.bz2
 tar jxf libksba-1.4.0.tar.bz2
@@ -34,7 +29,6 @@ cd libksba-1.4.0
 make
 sudo make install
 cd ..
-
 echo "Install Libassuan"
 wget https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.3.tar.bz2
 tar jxf libassuan-2.5.3.tar.bz2
@@ -43,7 +37,6 @@ cd libassuan-2.5.3
 make
 sudo make install
 cd ..
-
 echo "Install Ntbtls"
 wget https://www.gnupg.org/ftp/gcrypt/ntbtls/ntbtls-0.2.0.tar.bz2
 tar jxf ntbtls-0.2.0.tar.bz2
@@ -52,7 +45,6 @@ cd ntbtls-0.2.0
 make
 sudo make install
 cd ..
-
 echo "Install Npth"
 wget https://www.gnupg.org/ftp/gcrypt/npth/npth-1.6.tar.bz2
 tar jxf npth-1.6.tar.bz2
@@ -61,11 +53,9 @@ cd npth-1.6
 make
 sudo make install
 cd ..
-
 echo "Configure Dynamic Linker"
 echo 'include /usr/local/lib/' | sudo tee -a /etc/ld.so.conf
 sudo ldconfig -v
-
 echo "Install GnuPG"
 wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.23.tar.bz2
 tar jxf gnupg-2.2.23.tar.bz2
@@ -74,27 +64,20 @@ cd gnupg-2.2.23/
 make
 sudo make install
 cd ..
-
 echo "Install Pinentry"
 sudo apt-get install pinentry-curses
 echo 'pinentry-program /usr/bin/pinentry-curses' | tee -a ~/.gnupg/gpg-agent.conf
 gpg-connect-agent reloadagent /bye
-
 echo "Go to parent folder"
 cd ..
-
 echo "gpg version"
 gpg --version
-
 echo "Import gpg keys"
 gpg --batch --import all.gpg
-
 echo "List gpg keys"
 gpg --list-keys
-
 echo "List secret gpg keys"
 gpg --list-secret-keys
-
 echo "gpg.conf"
 file=~/.gnupg/gpg.conf
 echo "use-agent" >$file
@@ -102,19 +85,14 @@ echo "pinentry-mode loopback" >>$file
 echo "default-key $GPG_DEFAULT_KEY" >>$file
 echo "passphrase $GPG_PASSPHRASE" >>$file
 cat $file
-
 echo "gpg-agent.conf"
 echo "allow-loopback-pinentry" >~/.gnupg/gpg-agent.conf
 cat ~/.gnupg/gpg-agent.conf
 echo RELOADAGENT | gpg-connect-agent
-
 echo "Installing and configuration gpg success..."
-
 echo "Installing Ruby environment..."
 rvm use 2.6.5 --install --fuzzy
 gem update --system
-
 echo "Installing Jekyll..."
 gem install jekyll -v 4
-
 echo "Installing Ruby and Jekyll success..."
